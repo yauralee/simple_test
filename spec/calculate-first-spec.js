40f72ad8bce2ac548cea4b350354e51a432b4e63
raingxm <raingxm@163.com>
2015-11-29 20:09:34 +0800

    add test, broken calculate_first.js functionality

--- spec/calculate-first-spec.js
+++ spec/calculate-first-spec.js
@@ -2,6 +2,7 @@ describe('single_element_spec', function() {
 
   var collection_a = [1, 2, 3, 2, 5, 6, 21, 43, 12, 5];
   var collection_b = [11, 11, 22, 11, 33, 11];
+  var collection_pi = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 1, 4];
 
   it('第偶数个元素中，选出不重复的元素', function() {
     var result = single_element(collection_a);
@@ -11,5 +12,10 @@ describe('single_element_spec', function() {
   it('第偶数个元素中，选出不重复的元素', function() {
     var result = single_element(collection_b);
     expect(result).toEqual([]);
+  });
+
+  it('第偶数个元素中，选出不重复的元素, 我是倒霉蛋挂掉的测试', function() {
+    var result = single_element(collection_pi);
+    expect(result).toEqual([9, 6]);
   });
 });
